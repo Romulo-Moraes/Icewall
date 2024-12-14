f11c51f6aa3a64b1b876ba3f41d6c093850d4e8c
Romulo-Moraes <romulo.moraes.dev@gmail.com>
2024-12-13 23:49:34 -0300

    Implement the sentinel module

--- includes/sentinel.h
+++ includes/sentinel.h
@@ -1,9 +1,14 @@
 #ifndef __SENTINEL_GUARD__
 #define __SENTINEL_GUARD__
 
-void add_in_rule();
-void add_out_rule();
-void test_packet();
-void set_policy();
+#define DIRECTION_IN 1
+#define DIRECTION_OUT 2
+
+typedef unsigned char direction;
+
+void init_sentinel();
+long add_firewall_rule(struct rule_description rule, direction dir);
+long test_packet(struct packet pckt, direction dir, action *act_out);
+long set_policy(policy policy, direction dir);
 
 #endif
--- src/sentinel.c
+++ src/sentinel.c
@@ -3,20 +3,75 @@
 
 static struct rule_list_head in;
 static struct rule_list_head out;
-static policy dft_policy = POLICY_ACCEPT;
+static policy in_policy = POLICY_ACCEPT;
+static policy out_policy = POLICY_ACCEPT;
 
-void add_in_rule() {
-
+void init_sentinel() {
+    init_rule_list(&in, in_policy);
+    init_rule_list(&out, out_policy);
 }
 
-void add_out_rule() {
+/**
+ * add a new rule to the firewall
+ * @param rule the rule that will be added
+ * @param dir the rule's direction
+ * @returns -EINVAL, -ENOMEM on failure, 0 on success
+ */
+long add_firewall_rule(struct rule_description rule, direction dir) {
+    opstatus stt;
 
+    switch (dir) {
+        case DIRECTION_IN:
+            stt = add_rule(&in, rule);
+            break;
+        case DIRECTION_OUT:
+            stt = add_rule(&out, rule);
+            break;
+        default:
+            return -EINVAL;
+            break;
+    }
+
+    if (stt == MEM_FAILURE) {
+        return -ENOMEM;
+    }
+
+    return 0;
 }
 
-void test_packet() {
+/*
+ * test a packet against the specified ruleset
+ * @param pckt the incoming or outgoing packet
+ * @param dir the packet direction (DIRECTION_IN or DIRECTION_OUT)
+ * @param act_out the action to be performed based on the given packet and direction
+ * @returns -EINVAL if dir is not known, 0 otherwise, that means success
+ */
+long test_packet(struct packet pckt, direction dir, action *act_out) {
+    if (dir == DIRECTION_IN) {
+        *act_out = test_against_ruleset(&in, pckt, in_policy);
+    } else if (dir == DIRECTION_OUT) {
+        *act_out = test_against_ruleset(&out, pckt, out_policy);
+    } else {
+        return -EINVAL;
+    }
 
+    return 0;
 }
 
-void set_policy() {
+/*
+ * set the policy of incoming and outgoing packets
+ * @param policy the new policy to be used
+ * @param dir set policy to incoming or outgoing packets
+ * @returns -EINVAL if direction is not known, 0 otherwise, that means success
+ */
+long set_policy(policy policy, direction dir) {
+    if (dir == DIRECTION_IN) {
+        in_policy = policy;
+    } else if (dir == DIRECTION_OUT) {
+        out_policy = policy;
+    } else {
+        return -EINVAL;
+    }
 
+    return 0;
 }
