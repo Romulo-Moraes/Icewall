ad57431dbac4f4d2e9c74d6fc261c49d82797abf
Romulo-Moraes <romulo.moraes.dev@gmail.com>
2024-12-13 22:09:11 -0300

    Partially implement the sentinel module

---
+++ includes/sentinel.h
@@ -0,0 +1,9 @@
+#ifndef __SENTINEL_GUARD__
+#define __SENTINEL_GUARD__
+
+void add_in_rule();
+void add_out_rule();
+void test_packet();
+void set_policy();
+
+#endif
---
+++ src/sentinel.c
@@ -0,0 +1,22 @@
+#include "../includes/sentinel.h"
+#include "../includes/rules.h"
+
+static struct rule_list_head in;
+static struct rule_list_head out;
+static policy dft_policy = POLICY_ACCEPT;
+
+void add_in_rule() {
+
+}
+
+void add_out_rule() {
+
+}
+
+void test_packet() {
+
+}
+
+void set_policy() {
+
+}
